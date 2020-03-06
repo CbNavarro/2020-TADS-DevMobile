0a9f3716296aff1b6acd48ce2ff82cc467836ef4
Aluno <lucasnavarro103@gmail.com>
2020-03-05 21:20:34 -0300

    Modulos, componentes, bibliotecas, revisao prova

---
+++ EstudarProva/src/app/cliente/cliente.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { SalvarComponente } from './cliente-salvar/salvar.component';
+
+@NgModule({
+  declarations: [
+    SalvarComponente
+  ],
+  imports: [
+    SalvarComponente
+  ]
+})
+export class ClienteModule {
+
+}
